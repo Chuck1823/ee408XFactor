b0af33902096ca47c7596f0a43c859b859954e26
Charles Antoine Malenfant <malenf@clarkson.edu>
2017-11-17 16:47:37 -0500

    First legit commit with app files, finally!

---
+++ app/src/main/java/edu/clarkson/chaz/xfactor/TryItOut.java
@@ -0,0 +1,13 @@
+package edu.clarkson.chaz.xfactor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TryItOut extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_try_it_out);
+    }
+}
