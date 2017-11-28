5f5912436ecc7113e44f313c481489f5603f4e02
letaylo <letaylo@clarkson.edu>
2017-11-27 23:49:44 -0500

    Implementation version
    Project Implementation, with basic implementation of each activity

--- app/src/main/java/edu/clarkson/chaz/xfactor/TestYourSkills.java
+++ app/src/main/java/edu/clarkson/chaz/xfactor/TestYourSkills.java
@@ -11,7 +11,7 @@ import java.util.Random;
 
 public class TestYourSkills extends AppCompatActivity {
 
-    private Button a1, a2, a3, a4, backButton;
+    private Button a1, a2, a3, a4, backButton, nextQuestion;
     private int int1 = 0;
     private int int2 = 0;
     private int answer = 0;
@@ -22,23 +22,17 @@ public class TestYourSkills extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_your_skills);
 
+        final Random randGenerator = new Random();
+
         a1 = (Button) findViewById(R.id.a1);
         a2 = (Button) findViewById(R.id.a2);
         a3 = (Button) findViewById(R.id.a3);
         a4 = (Button) findViewById(R.id.a4);
-
+        nextQuestion = (Button) findViewById(R.id.nextQuestion);
         backButton = (Button) findViewById(R.id.back_button);
-
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(TestYourSkills.this, Menu.class));
-            }
-        });
-
+        final TextView dialog = (TextView) findViewById(R.id.dialog);
         TextView question = (TextView) findViewById(R.id.question);
 
-        Random randGenerator = new Random();
         int1 = randGenerator.nextInt(101);
         int2 = randGenerator.nextInt(101);
 
@@ -69,5 +63,97 @@ public class TestYourSkills extends AppCompatActivity {
             a3.setText(String.valueOf(randGenerator.nextInt(10001)));
             a4.setText(String.valueOf(answer));
         }
+
+        a1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Integer.parseInt(a1.getText().toString()) == answer) {
+                    dialog.setText("Correct!");
+                } else {
+                    dialog.setText("Incorrect!");
+                }
+            }
+        });
+
+        a2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Integer.parseInt(a2.getText().toString()) == answer) {
+                    dialog.setText("Correct!");
+                } else {
+                    dialog.setText("Incorrect!");
+                }
+            }
+        });
+
+        a3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Integer.parseInt(a3.getText().toString()) == answer) {
+                    dialog.setText("Correct!");
+                } else {
+                    dialog.setText("Incorrect!");
+                }
+            }
+        });
+
+        a4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Integer.parseInt(a4.getText().toString()) == answer) {
+                    dialog.setText("Correct!");
+                } else {
+                    dialog.setText("Incorrect!");
+                }
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(TestYourSkills.this, Menu.class));
+            }
+        });
+
+        nextQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int1 = randGenerator.nextInt(101);
+                int2 = randGenerator.nextInt(101);
+
+                TextView question = (TextView) findViewById(R.id.question);
+                TextView dialog = (TextView) findViewById(R.id.dialog);
+
+                dialog.setText("");
+
+                answer = int1 * int2;
+
+                question.setText("What is the product of " + int1 + " and " + int2 + "?");
+
+                rotation = randGenerator.nextInt(4);
+
+                if(rotation == 0) {
+                    a1.setText(String.valueOf(answer));
+                    a2.setText(String.valueOf(randGenerator.nextInt(10001)));
+                    a3.setText(String.valueOf(randGenerator.nextInt(10001)));
+                    a4.setText(String.valueOf(randGenerator.nextInt(10001)));
+                } else if (rotation == 1) {
+                    a1.setText(String.valueOf(randGenerator.nextInt(10001)));
+                    a2.setText(String.valueOf(answer));
+                    a3.setText(String.valueOf(randGenerator.nextInt(10001)));
+                    a4.setText(String.valueOf(randGenerator.nextInt(10001)));
+                } else if (rotation == 2) {
+                    a1.setText(String.valueOf(randGenerator.nextInt(10001)));
+                    a2.setText(String.valueOf(randGenerator.nextInt(10001)));
+                    a3.setText(String.valueOf(answer));
+                    a4.setText(String.valueOf(randGenerator.nextInt(10001)));
+                } else if (rotation == 3) {
+                    a1.setText(String.valueOf(randGenerator.nextInt(10001)));
+                    a2.setText(String.valueOf(randGenerator.nextInt(10001)));
+                    a3.setText(String.valueOf(randGenerator.nextInt(10001)));
+                    a4.setText(String.valueOf(answer));
+                }
+            }
+        });
     }
 }
